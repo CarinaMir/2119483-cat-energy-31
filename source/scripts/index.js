bd0bc03c2af300cde65ad215412596d09b21a02d
Karina <alt.k@yandex.ru>
2024-05-23 00:34:32 +0300

    Добавлена карта

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -2,6 +2,9 @@ const navToggle = document.querySelector('.header__toggle');
 const navBottonMenu = document.querySelector('.header__toggle--closed');
 const navMain = document.querySelector('.navigation');
 
+document.querySelector('.header--no-js').classList.remove('header--no-js');
+document.querySelector('.contact__map--no-js').classList.remove('contact__map--no-js');
+
 navToggle.addEventListener('click', () => {
   if (navBottonMenu.classList.contains('header__toggle--closed')) {
     navBottonMenu.classList.remove('header__toggle--closed');
